e633e61826262903b140df2106d74c06f944a7dc
nabyss <1961542078@qq.com>
2016-11-22 21:39:49 +0800

    编写首页推荐页面相关代码（adapter、fragment等）

---
+++ MyApplication/app/src/main/java/com/example/administrator/domain/Data_Recommend.java
@@ -0,0 +1,73 @@
+package com.example.administrator.domain;
+
+/**
+ * 推荐表类
+ * Created by 王晨阳 on 2016/11/22.
+ */
+
+public class Data_Recommend {
+    private long Id;    //推荐表id
+    private String RecipesId;   //食谱id
+    private String Image;   //食谱图片URL
+    private String Name;   //食谱名称
+    private String Introduction;   //食谱简介
+
+    /**
+     * 带参构造方法
+     * @param id
+     * @param image
+     * @param introduction
+     * @param name
+     * @param recipesId
+     */
+    public Data_Recommend(long id, String recipesId, String image, String name, String introduction) {
+        Id = id;
+        RecipesId = recipesId;
+        Image = image;
+        Name = name;
+        Introduction = introduction;
+    }
+
+    /**
+     * getter,setter 方法
+     */
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
+
+    public String getIntroduction() {
+        return Introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        Introduction = introduction;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getRecipesId() {
+        return RecipesId;
+    }
+
+    public void setRecipesId(String recipesId) {
+        RecipesId = recipesId;
+    }
+}
--- MyApplication/app/src/main/java/com/example/administrator/ui/VerticalTextView.java
+++ MyApplication/app/src/main/java/com/example/administrator/ui/VerticalTextView.java
@@ -2,6 +2,7 @@ package com.example.administrator.ui;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.widget.TextView;
@@ -13,39 +14,55 @@ import android.widget.TextView;
 public class VerticalTextView extends TextView {
     final boolean topDown;
 
+    public VerticalTextView(Context context, AttributeSet attrs) {
 
-    public VerticalTextView(Context context, AttributeSet attrs){
         super(context, attrs);
+
         final int gravity = getGravity();
-        if(Gravity.isVertical(gravity) && (gravity&Gravity.VERTICAL_GRAVITY_MASK) == Gravity.BOTTOM) {
-            setGravity((gravity&Gravity.HORIZONTAL_GRAVITY_MASK) | Gravity.TOP);
+
+        if (Gravity.isVertical(gravity)
+                && (gravity & Gravity.VERTICAL_GRAVITY_MASK) == Gravity.BOTTOM) {
+
+            setGravity((gravity & Gravity.HORIZONTAL_GRAVITY_MASK) | Gravity.TOP);
             topDown = false;
-        }else
+        }
+        else {
+
             topDown = true;
+        }
     }
 
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
         super.onMeasure(heightMeasureSpec, widthMeasureSpec);
+
         setMeasuredDimension(getMeasuredHeight(), getMeasuredWidth());
     }
 
-
     @Override
-    protected boolean setFrame(int l, int t, int r, int b){
-        return super.setFrame(l, t, l+(b-t), t+(r-l));
-    }
+    protected void onDraw(Canvas canvas) {
 
-    @Override
-    public void draw(Canvas canvas){
-        if(topDown){
-            canvas.translate(getHeight(), 0);
+        TextPaint textPaint = getPaint();
+        textPaint.setColor(getCurrentTextColor());
+        textPaint.drawableState = getDrawableState();
+
+        canvas.save();
+
+        if (topDown) {
+
+            canvas.translate(getWidth(), 0);
             canvas.rotate(90);
-        }else {
-            canvas.translate(0, getWidth());
+        }
+        else {
+
+            canvas.translate(0, getHeight());
             canvas.rotate(-90);
         }
-        canvas.clipRect(0, 0, getWidth(), getHeight(), android.graphics.Region.Op.REPLACE);
-        super.draw(canvas);
+
+        canvas.translate(getCompoundPaddingLeft(), getExtendedPaddingTop());
+
+        getLayout().draw(canvas);
+        canvas.restore();
     }
 }
