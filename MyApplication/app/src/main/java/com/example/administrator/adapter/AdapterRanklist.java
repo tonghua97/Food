c1b2a3621629bbc9b41008cd7d2cc5f89db2fb9e
jiangjiani <951329327@qq.com>
2016-11-24 08:28:12 +0800

    修复DataRanklist

---
+++ MyApplication/app/src/main/java/com/example/administrator/adapter/AdapterRanklist.java
@@ -0,0 +1,67 @@
+package com.example.administrator.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.administrator.domain.DataRanklist;
+import com.example.administrator.myapplication.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Sophia on 2016/11/24.
+ */
+public class AdapterRanklist extends BaseAdapter {
+    private Context context;
+    private List<DataRanklist> lrl = new ArrayList<>();
+
+    public AdapterRanklist(List<DataRanklist> lrl,Context context){
+        lrl = lrl;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return lrl.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return lrl.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return lrl.get(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(null == convertView){
+            convertView = LayoutInflater.from(context).inflate(R.layout.listview_item_ranklist,null);
+        }
+        //食谱收藏排行
+        TextView Tv_rank_id = (TextView)convertView.findViewById(R.id.Tv_rank_id);
+        //获取排行待解决 2016/11/23 ginger
+
+        //食谱收藏数 Tv_ranklist_collect
+        TextView Tv_ranklist_collect = (TextView)convertView.findViewById(R.id.Tv_ranklist_collect);
+        Tv_ranklist_collect.setText(lrl.get(position).getNum());
+
+        //食谱图片 Iv_ranklist_recipeimg
+        ImageView Iv_ranklist_recipeimg = (ImageView)convertView.findViewById((R.id.Iv_ranklist_recipeimg));
+        String imgurl = lrl.get(position).getImage();
+
+        //食谱名 Tv_ranklist_recipename
+        TextView Tv_pickfun_title = (TextView) convertView.findViewById(R.id.Tv_pickfun_title);
+        Tv_pickfun_title.setText(lrl.get(position).getName());
+
+        return convertView;
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/administrator/domain/DataRanklist.java
@@ -0,0 +1,70 @@
+package com.example.administrator.domain;
+
+/**
+ * Created by Sophia on 2016/11/24.
+ */
+public class DataRanklist {
+    private long Id;            //推荐表id
+    private String RecipesId;   //食谱id
+    private String Image;       //食谱图片URL Iv_ranklist_recipeimg
+    private String Name;        //食谱名称 Tv_ranklist_recipename
+    private int num;            //食谱收藏数 Tv_ranklist_collect
+    private int rank;           //食谱排行 Tv_rank_id
+
+    public DataRanklist(long id, int rank, int num, String image, String name, String recipesId) {
+        Id = id;
+        this.rank = rank;
+        this.num = num;
+        Image = image;
+        Name = name;
+        RecipesId = recipesId;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getRecipesId() {
+        return RecipesId;
+    }
+
+    public void setRecipesId(String recipesId) {
+        RecipesId = recipesId;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+}
