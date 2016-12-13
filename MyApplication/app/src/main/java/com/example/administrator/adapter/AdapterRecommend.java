09e592b9c77c69dd72c601e4fad70153b39f1634
nabyss <1961542078@qq.com>
2016-12-13 17:27:26 +0800

    修复bug

--- MyApplication/app/src/main/java/com/example/administrator/adapter/AdapterRecommend.java
+++ MyApplication/app/src/main/java/com/example/administrator/adapter/AdapterRecommend.java
@@ -1,6 +1,7 @@
 package com.example.administrator.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -54,8 +55,10 @@ public class AdapterRecommend extends BaseAdapter{
         if (view == null) {
             if (i%2 == 0) {
                 view = LayoutInflater.from(mContext).inflate(R.layout.listview_item_recommend_right, null);
+                Log.e("=======", i+"+right" );
             }else {
                 view = LayoutInflater.from(mContext).inflate(R.layout.listview_item_recommend_left, null);
+                Log.e("=======", i+"+left" );
             }
         }
         ImageView Rec_Image = (ImageView)view.findViewById(R.id.Iv_recommend_image);
--- MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -210,7 +210,7 @@ public class MainActivity extends Activity {
                     .normalImageRes(iamgeResource[i])  //设置按钮图片
                     .normalText(buttonText[i])  //设置文字
                     .textSize(18)  //设置字体大小
-                    .textWidth(300)  //设置字体区域宽度
+                    .textWidth(150)  //设置字体区域宽度
                     .textHeight(80)  //设置字体区域高度
                     .normalColor(Color.argb(255, 249, 197, 21))  //设置按钮颜色
                     .highlightedColor(Color.RED)  //设置按钮点击颜色
