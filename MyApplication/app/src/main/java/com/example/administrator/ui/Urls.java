e0938e50f3c72a49147686ee28702eebc4369462
JingJerr <542567635@qq.com>
2016-12-20 15:26:07 +0800

    优化个人设置界面

--- MyApplication/app/src/main/java/com/example/administrator/ui/Urls.java
+++ MyApplication/app/src/main/java/com/example/administrator/ui/Urls.java
@@ -4,7 +4,7 @@ package com.example.administrator.ui;
  * Created by Administrator on 2016/12/13.
  */
 public class Urls {
-    public static String mIp = "10.7.81.44";
+    public static String mIp = "www.nabyss.cn/foodfun";
     //按名称搜索URL
     public static String urlSearch = "http://" + mIp + "/http/searchByRecipesName";
     public static String urlCommend = "http://" + mIp + "/http/getCommend";
